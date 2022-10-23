db0b4d6526ec331fc18938d79137c818b1e944a1
Griza20 <amargrizovic5@gmail.com>
2022-10-23 16:04:10 +0200

    new comment

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -2,12 +2,12 @@ package ba.unsa.etf.rpr;
 
 /**
  * Hello world!
- *
+ * comment by Amar
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Dobro došli na RPR!" );
     }
 }
